6420bf4dd77c55c506dda0b3d67110ceb025c73c
Eric Swanson <ericis@users.noreply.github.com>
2020-11-18 20:13:01 -0500

    feat: prettier linting (#12)

--- package.test.js
+++ package.test.js
@@ -1,15 +1,17 @@
 const packageConfig = require('./package.json');
 
 test('package.json scripts', async () => {
-  expect(packageConfig.scripts).toBeTruthy();
-  expect(packageConfig.scripts.test).toBe('jest');
-  expect(packageConfig.scripts.commit).toBe('commit');
+  expect(packageConfig.scripts).toBeUndefined();
 });
 
 test('package.json husky', async () => {
   expect(packageConfig.husky).toBeTruthy();
 
   expect(packageConfig.husky.hooks).toBeTruthy();
+
+  expect(packageConfig.husky.hooks['pre-commit']).toBeTruthy();
+  expect(packageConfig.husky.hooks['pre-commit']).toBe('lint-staged');
+
   expect(packageConfig.husky.hooks['commit-msg']).toBeTruthy();
   expect(packageConfig.husky.hooks['commit-msg']).toBe(
     'commitlint -E HUSKY_GIT_PARAMS'
@@ -17,18 +19,19 @@ test('package.json husky', async () => {
 });
 
 test('package.json devDepencencies', async () => {
-  expect(packageConfig.devDependencies).toBeTruthy();
-
-  // testing
-  expect(packageConfig.devDependencies['@types/jest']).toBeTruthy();
-  expect(packageConfig.devDependencies['jest']).toBeTruthy();
-
-  // git ignore
-  expect(packageConfig.devDependencies['parse-gitignore']).toBeTruthy();
-
-  // editor configuration
-  expect(packageConfig.devDependencies['editorconfig']).toBeTruthy();
-
-  // documentation
-  expect(packageConfig.devDependencies['markdown-it']).toBeTruthy();
+  expect(packageConfig.devDependencies).toEqual({
+    '@commitlint/cli': '^11.0.0',
+    '@commitlint/config-conventional': '^11.0.0',
+    '@commitlint/prompt-cli': '^11.0.0',
+    '@types/jest': '^26.0.15',
+    editorconfig: '^0.15.3',
+    husky: '^4.3.0',
+    jest: '^26.6.3',
+    'lint-staged': '^10.5.1',
+    'markdown-it': '^12.0.2',
+    mrm: '^2.5.12',
+    'parse-gitignore': '^1.0.1',
+    prettier: '^2.1.2',
+    yaml: '^1.10.0',
+  });
 });
