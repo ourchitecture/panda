37ac16ceb6ff575600a7d4df4ed5718f74191fde
Eric Swanson <ericis@users.noreply.github.com>
2020-12-16 18:21:34 -0500

    fix(ci/cli): npm and yarn registry

--- src/scripts/node/yarn/init.mk
+++ src/scripts/node/yarn/init.mk
@@ -1,8 +1,17 @@
 .PHONY: yarn-init
 yarn-init: require-root require-node
+ifdef YARN_REGISTRY_URL
+	@docker run --rm -t \
+		--name $(node_docker_name_prefix)-$@ \
+		-v $(my_dir_path):/app \
+		-w /app \
+		$(node_docker) \
+		yarn --registry "$(YARN_REGISTRY_URL)"
+else
 	@docker run --rm -t \
 		--name $(node_docker_name_prefix)-$@ \
 		-v $(my_dir_path):/app \
 		-w /app \
 		$(node_docker) \
 		yarn
+endif
