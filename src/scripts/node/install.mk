5b88ed90c9692c8e33c964c6408059b926cca482
Eric Swanson <ericis@users.noreply.github.com>
2020-12-17 21:08:58 -0500

    fix: node install

--- src/scripts/node/install.mk
+++ src/scripts/node/install.mk
@@ -2,7 +2,8 @@
 node-install: require-root require-node
 	@docker run --rm -t \
 		--name $(node_docker_name_prefix)-$@ \
-		-v $(scripts_dir_path):/our \
+		-v $(my_dir_path):/app \
+		-w /app \
 		--network host \
 		$(node_docker) \
 		npm install
